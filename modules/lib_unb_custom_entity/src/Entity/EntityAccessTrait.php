b7f84eb08b4180beaae55706a13bd5f15a7cd3c8
Patrick Schilf <pschilf@unb.ca>
2020-01-08 14:12:10 -0400

    EntityAccessTrait: load current user if none explicitly given

--- modules/lib_unb_custom_entity/src/Entity/EntityAccessTrait.php
+++ modules/lib_unb_custom_entity/src/Entity/EntityAccessTrait.php
@@ -9,13 +9,20 @@ trait EntityAccessTrait {
 
   public function access($operation, AccountInterface $account = NULL, $return_as_object = FALSE) {
     /** @var \Drupal\Core\Access\AccessResultInterface $access */
-    $access = parent::access($operation, $account, $return_as_object);
+    $access = parent::access($operation, $account, TRUE);
+
     if (!$access->isForbidden()) {
+      if (!$account) {
+        $account = \Drupal::currentUser();
+      }
+
       /** @var \Drupal\Core\Entity\EntityInterface $this */
       $entity_type_id = $this->getEntityTypeId();
       $permission = "$operation $entity_type_id entities";
       $access = AccessResult::allowedIfHasPermission($account, $permission);
     }
-    return $access;
+
+    return $return_as_object ? $access : $access->isAllowed();
   }
+
 }
