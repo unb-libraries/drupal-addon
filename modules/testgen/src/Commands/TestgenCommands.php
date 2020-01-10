eb8d4ff665c08dbdc8f1c005e11c96f04c24530a
Patrick Schilf <pschilf@unb.ca>
2020-01-10 14:02:16 -0400

    IN-198 Generate module tests

--- modules/testgen/src/Commands/TestgenCommands.php
+++ modules/testgen/src/Commands/TestgenCommands.php
@@ -6,7 +6,8 @@ use Drush\Commands\DrushCommands;
 
 class TestgenCommands extends DrushCommands {
 
-  const GLOBAL_TEST_ROOT = '/app/tests/behat/features/example/';
+  const LOCAL_TEST_ROOT = '/tests/behat/features/example';
+  const GLOBAL_TEST_ROOT = '/app' . self::LOCAL_TEST_ROOT;
 
   /**
    * Test generator service.
@@ -16,26 +17,58 @@ class TestgenCommands extends DrushCommands {
   protected $generator;
 
   /**
+   * Module handler service.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
    * Retrieve the test generator service.
    *
    * @return \TestGen\generate\TestGenerator
    *   A test generator service instance.
    */
   public function generator() {
-    if (!isset($this->generator)) {
-      $this->generator = \Drupal::service('testgen.generator');
-    }
     return $this->generator;
+  }
+
+  /**
+   * Retrieves the module handler service.
+   *
+   * @return \Drupal\Core\Extension\ModuleHandlerInterface
+   *   A module handler service instance.
+   */
+  public function moduleHandler() {
+    return $this->moduleHandler;
+  }
+
+  /**
+   * Creates the TestgenCommands instance.
+   */
+  public function __construct() {
+    $this->generator = \Drupal::service('testgen.generator');
+    $this->moduleHandler = \Drupal::moduleHandler();
   }
 
   /**
    * Generates test files in the GLOBAL_TEST_ROOT folder.
    *
+   * @param string $module_name
+   *   Name of the module for which to generate test cases.
+   *
    * @command testgen:generate
    * @aliases tgen
    */
-  public function generate() {
-    $this->generator()->generate(self::GLOBAL_TEST_ROOT);
+  public function generate($module_name = '') {
+    if (!empty($module_name)) {
+      $module = $this->moduleHandler()->getModule($module_name);
+      $module_test_path = $module->getPath() . self::LOCAL_TEST_ROOT;
+      $this->generator()->generate($module_test_path);
+    }
+    else {
+      $this->generator()->generate(self::GLOBAL_TEST_ROOT);
+    }
   }
 
 }
