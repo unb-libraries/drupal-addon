192265633cde80d232757bd6f9ce288817237259
Patrick Schilf <pschilf@unb.ca>
2020-01-14 14:25:55 -0400

    Document and refactor JsonController

--- modules/lib_unb_custom_entity/src/Controller/JsonController.php
+++ modules/lib_unb_custom_entity/src/Controller/JsonController.php
@@ -7,41 +7,111 @@ use Drupal\Core\Entity\EntityInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Controller which responds to requests that require "_format=json".
+ *
+ * @package Drupal\lib_unb_custom_entity\Controller
+ */
 class JsonController extends ControllerBase {
 
   protected const FORMAT = 'json';
 
+  /**
+   * Retrieve the storage handler for the given entity type.
+   *
+   * @param $entity_type
+   *   The entity type for which to retrieve a storage handler.
+   *
+   * @return \Drupal\Core\Entity\EntityStorageInterface
+   *   An instance of EntityStorageInterface.
+   */
   protected function getStorage($entity_type) {
     return \Drupal::entityTypeManager()->getStorage($entity_type);
   }
 
+  /**
+   * Retrieve the given entity.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity to retrieve.
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The current request.
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   *   An instance of JsonResponse.
+   */
   public function view(EntityInterface $entity, Request $request) {
     $json = $entity->toArray() + [
       'url' => $entity->toUrl('canonical', [
         'format' => self::FORMAT
       ])->toString(),
     ];
-    return new JsonResponse($json, 200);
+
+    return $this->respond($json);
   }
 
+  /**
+   * Create a new entity of the given type.
+   *
+   * @param string $entity_type
+   *   The entity type of which to create an instance.
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The current request.
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   *   An instance of JsonResponse.
+   */
   public function add($entity_type, Request $request) {
-    return new JsonResponse([
+    return $this->respond([
       'message' => 'Nothing to see or do here.'
-    ], 200);
+    ]);
   }
 
+  /**
+   * Update the given entity.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity to update.
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The current request.
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   *   An instance of JsonResponse.
+   */
   public function edit(EntityInterface $entity, Request $request) {
-    return new JsonResponse([
+    return $this->respond([
       'message' => 'Nothing to see or do here.'
-    ], 200);
+    ]);
   }
 
+  /**
+   * Delete the given entity.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity to delete.
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The current request.
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   *   An instance of JsonResponse.
+   */
   public function delete(EntityInterface $entity, Request $request) {
-    return new JsonResponse([
+    return $this->respond([
       'message' => 'Nothing to see or do here.'
-    ], 200);
+    ]);
   }
 
+  /**
+   * List all entities of the given type in JSON format.
+   *
+   * @param string $entity_type
+   *   The entity type to list.
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The current request.
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   *   An instance of JsonResponse.
+   */
   public function list($entity_type, Request $request) {
     foreach ($this->getStorage($entity_type)->loadMultiple() as $entity) {
       $json[] = [
@@ -52,8 +122,24 @@ class JsonController extends ControllerBase {
         ])->toString(),
       ];
     }
+    return $this->respond($json);
+  }
 
-    return new JsonResponse($json, 200);
+  /**
+   * Build a respond object.
+   *
+   * @param $data
+   *   The data to contain in the response.
+   * @param int $status
+   *   The HTTP respond code.
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   *   An instance of JsonResponse.
+   */
+  protected function respond($data, $status = 200) {
+    return new JsonResponse($data, $status);
+  }
+
   }
 
 }
