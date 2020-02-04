cd60bfecc20598ecbdbf6daa1268846457103b40
Patrick Schilf <pschilf@unb.ca>
2020-02-04 14:24:02 -0400

    URL generation
    Create entity URL based on whether the specific format is
    supported, i.e. whether a route provider exists

--- modules/lib_unb_custom_entity/src/Entity/ContentEntityResource.php
+++ modules/lib_unb_custom_entity/src/Entity/ContentEntityResource.php
@@ -17,12 +17,28 @@ abstract class ContentEntityResource extends ContentEntityBase {
   public function toUrl($rel = 'canonical', array $options = []) {
     /** @var \Drupal\Core\Url $url */
     $url = parent::toUrl($rel, $options);
-    if (array_key_exists('format', $options)) {
+    if (array_key_exists('format', $options) && $this->respondsTo($format = $options['format'])) {
       $url->setOption('query', [
-        '_format' => $options['format'],
+        '_format' => $format,
       ]);
     }
     return $url;
+  }
+
+  /**
+   * Whether the entity can be queried in the given format.
+   *
+   * @param string $format
+   *   The format, e.g. 'html' or 'json'.
+   *
+   * @return bool
+   *   TRUE if a route provider exists for the given format
+   *   and the entity. FALSE otherwise.
+   */
+  protected function respondsTo($format) {
+    $route_providers = $this->getEntityType()
+      ->getHandlerClasses()['route_provider'];
+    return array_key_exists($format, $route_providers);
   }
 
 }
