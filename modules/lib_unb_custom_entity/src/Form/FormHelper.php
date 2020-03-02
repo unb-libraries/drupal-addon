623c915fecdff9c143955ea0f69b71652f1ce25c
Patrick Schilf <pschilf@unb.ca>
2020-03-02 16:46:36 -0400

    Plugin labels form helper

--- modules/lib_unb_custom_entity/src/Form/FormHelper.php
+++ modules/lib_unb_custom_entity/src/Form/FormHelper.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\lib_unb_custom_entity\Form;
 
+use Drupal\Component\Plugin\PluginBase;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Form\FormHelper as CoreFormHelper;
 
@@ -25,6 +26,25 @@ class FormHelper extends CoreFormHelper {
     return array_map(function (EntityInterface $entity) {
       return $entity->label();
     }, $entities);
+  }
+
+  /**
+   * Convert an array of plugins or plugin definitions into an array of plugin labels.
+   *
+   * @param array $plugins
+   *   An array of plugins or plugin definitions, keyed by PLUGIN_ID.
+   *
+   * @return array
+   *   An array of the form PLUGIN_ID => PLUGIN_LABEL.
+   */
+  public static function pluginLabels(array $plugins) {
+    return array_map(function ($plugin) {
+      if ($plugin instanceof PluginBase) {
+        return $plugin->getPluginDefinition()['label'];
+      } else {
+        return $plugin['label'];
+      }
+    }, $plugins);
   }
 
 }
