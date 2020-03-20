f050a75a105dbdbaa74380ddb532716ac5d307d2
Patrick Schilf <pschilf@unb.ca>
2020-03-20 12:56:18 -0300

    INTRANET-98 Make 'created' and 'changed' default content entity fields

--- modules/lib_unb_custom_entity/src/Entity/ContentEntityBase.php
+++ modules/lib_unb_custom_entity/src/Entity/ContentEntityBase.php
@@ -3,6 +3,9 @@
 namespace Drupal\lib_unb_custom_entity\Entity;
 
 use \Drupal\Core\Entity\ContentEntityBase as DefaultContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\datetime_plus\DependencyInjection\UserTimeTrait;
 
 /**
  * Enhances Drupal's original ContentEntityBase class.
@@ -10,6 +13,11 @@ use \Drupal\Core\Entity\ContentEntityBase as DefaultContentEntityBase;
  * @package Drupal\lib_unb_custom_entity\Entity
  */
 abstract class ContentEntityBase extends DefaultContentEntityBase {
+
+  use UserTimeTrait;
+
+  const FIELD_CREATED = 'created';
+  const FIELD_CHANGED = 'changed';
 
   /**
    * Loads one or more entities and returns their labels.
@@ -24,6 +32,49 @@ abstract class ContentEntityBase extends DefaultContentEntityBase {
     return array_map(function (ContentEntityBase $entity) {
       return $entity->label();
     }, self::loadMultiple($ids));
+  }
+
+  /**
+   * Retrieve the entity's creation date and time.
+   *
+   * The timezone is set to the currently logged-in user's.
+   *
+   * @return \Drupal\datetime_plus\Datetime\DrupalDateTimePlus
+   *   A datetime object.
+   */
+  public function getCreated() {
+    return $this->userTime()
+      ->createFromTimestamp($this->get(self::FIELD_CREATED)->value);
+  }
+
+  /**
+   * Retrieve the entity's date and time of its most recent edit.
+   *
+   * The timezone is set to the currently logged-in user's.
+   *
+   * @return \Drupal\datetime_plus\Datetime\DrupalDateTimePlus
+   *   A datetime object.
+   */
+  public function getChanged() {
+    return $this->userTime()
+      ->createFromTimestamp($this->get(self::FIELD_CHANGED)->value);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    $fields[self::FIELD_CREATED] = BaseFieldDefinition::create('created')
+      ->setLabel(t('Created'))
+      ->setDescription(t("Timestamp indicating the location's creation."));
+
+    $fields[self::FIELD_CHANGED] = BaseFieldDefinition::create('changed')
+      ->setLabel(t('Changed'))
+      ->setDescription(t("Timestamp indicating the location's last update."));
+
+    return $fields;
   }
 
 }
