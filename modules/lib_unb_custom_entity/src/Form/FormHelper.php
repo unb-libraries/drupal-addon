2a29892cd0d4399e1a989539f3270e6b64d3fa33
Patrick Schilf <pschilf@unb.ca>
2020-04-03 11:23:56 -0300

    Form helper: pluralize

--- modules/lib_unb_custom_entity/src/Form/FormHelper.php
+++ modules/lib_unb_custom_entity/src/Form/FormHelper.php
@@ -5,6 +5,7 @@ namespace Drupal\lib_unb_custom_entity\Form;
 use Drupal\Component\Plugin\PluginBase;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Form\FormHelper as CoreFormHelper;
+use Drupal\Core\StringTranslation\PluralTranslatableMarkup;
 
 /**
  * Provides form helpers in addition to Drupal's core FormHelper class.
@@ -45,6 +46,24 @@ class FormHelper extends CoreFormHelper {
         return $plugin['label'];
       }
     }, $plugins);
+  }
+
+  /**
+   * Render a singular or plural version of the input according to the given count.
+   *
+   * @param int $count
+   *   The number to determine whether singular or plural will be used.
+   * @param string $singular
+   *   The singular version of the to be rendered term.
+   * @param string $plural
+   *   The plural version of the to be rendered term.
+   *
+   * @return string
+   */
+  public static function pluralize($count, $singular, $plural) {
+    return sprintf('%s %s',
+      $count, new PluralTranslatableMarkup($count, $singular, $plural),
+    );
   }
 
 }
