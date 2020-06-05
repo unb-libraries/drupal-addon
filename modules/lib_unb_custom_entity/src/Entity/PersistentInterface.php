136b1e460ac204645a646dfe11f6d7d0bb58db06
Patrick Schilf <pschilf@unb.ca>
2020-06-04 23:48:19 -0300

    INTRANET-144 Create PersistentEntityInterface
    - Interface for non-deletable entities
    - to replace indestructable entity implementation

---
+++ modules/lib_unb_custom_entity/src/Entity/PersistentInterface.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\lib_unb_custom_entity\Entity;
+
+use Drupal\Core\Entity\EntityInterface;
+
+/**
+ * Interface for entities that should never be deleted.
+ *
+ * @package Drupal\lib_unb_custom_entity\Entity
+ */
+interface PersistentInterface extends EntityInterface {
+
+  /**
+   * Terminate the entity, e.g. mark as "deleted".
+   */
+  public function terminate();
+
+  /**
+   * Whether the entity is considered "deleted".
+   *
+   * @param int|null $timestamp
+   *   (optional) A UNIX timestamp. Set to determine the
+   *   entity's existence at the given time. Otherwise
+   *   the current existence will be determined.
+   *
+   * @return bool
+   *   TRUE if the entity has been "deleted". FALSE otherwise.
+   */
+  public function isTerminated($timestamp = NULL);
+
+  /**
+   * Truly removes an entity permanently.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   *   In case of failures an exception is thrown.
+   */
+  public function destroy();
+
+}
