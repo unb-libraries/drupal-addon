32e85fa8ea1e31f5b1a3aa65e0a24fba93474d06
Patrick Schilf <pschilf@unb.ca>
2020-08-14 14:49:17 -0300

    [lib_unb_custom_entity] Implement custom content entity storage handler

---
+++ modules/lib_unb_custom_entity/src/Entity/Storage/CustomContentEntityStorageInterface.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\lib_unb_custom_entity\Entity\Storage;
+
+use Drupal\Core\Entity\ContentEntityStorageInterface;
+
+/**
+ * Enhanced storage handler for content entities.
+ *
+ * @package Drupal\lib_unb_custom_entity\Entity\Storage
+ */
+interface CustomContentEntityStorageInterface extends ContentEntityStorageInterface {
+
+  /**
+   * Load entities that have not been changed since the given time.
+   *
+   * @param \Drupal\Core\Datetime\DrupalDateTime|string $datetime
+   *   A datetime object or string.
+   * @param \DateTimeZone|string|null $timezone
+   *   (optional) The timezone to which the given
+   *   datetime object is set. If none is provided the
+   *   storage timezone will be used.
+   *
+   * @return \Drupal\lib_unb_custom_entity\Entity\ContentEntityInterface[]
+   *   An array of content entities.
+   */
+  public function loadUnalteredSince($datetime, $timezone = NULL);
+
+  /**
+   * Load entities that have not been altered for the given time period.
+   *
+   * @param string $duration
+   *   A string indicating a time period, e.g. "3 days".
+   *
+   * @return \Drupal\lib_unb_custom_entity\Entity\ContentEntityInterface[]
+   *   An array of content entities.
+   */
+  public function loadUnalteredFor($duration);
+
+}
---
+++ modules/lib_unb_custom_entity/src/Entity/Storage/CustomSqlContentEntityStorage.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\lib_unb_custom_entity\Entity\Storage;
+
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\Core\Entity\Sql\SqlContentEntityStorage;
+use Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface;
+use Drupal\datetime_plus\Datetime\Timespan;
+use Drupal\datetime_plus\DependencyInjection\StorageTimeTrait;
+use Drupal\lib_unb_custom_entity\Entity\ContentEntityInterface;
+
+/**
+ * Enhanced content entity storage handler.
+ *
+ * @package Drupal\lib_unb_custom_entity\Entity\Storage
+ */
+class CustomSqlContentEntityStorage extends SqlContentEntityStorage implements CustomContentEntityStorageInterface {
+
+  use StorageTimeTrait;
+
+  /**
+   * {@inheritDoc}
+   */
+  public function loadUnalteredSince($datetime, $timezone = NULL) {
+    if (!$timezone) {
+      $timezone = DateTimeItemInterface::STORAGE_TIMEZONE;
+    }
+    if (is_string($datetime)) {
+      $datetime = new DrupalDateTime($datetime, $timezone);
+    }
+    $datetime->setTimezone($this->storageTime()->getTimeZone());
+
+    $entity_ids = $this->getQuery()
+      ->condition(ContentEntityInterface::CHANGED, $datetime->format(DateTimeItemInterface::DATETIME_STORAGE_FORMAT), '>=')
+      ->execute();
+    if (!empty($entity_ids)) {
+      return $this->loadMultiple($entity_ids);
+    }
+    return [];
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function loadUnalteredFor($duration) {
+    $datetime = $this->storageTime()
+      ->now()
+      ->minus(Timespan::createFromDateString($duration));
+    return $this->loadUnalteredSince($datetime);
+  }
+
+}
