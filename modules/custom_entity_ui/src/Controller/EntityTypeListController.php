e1ffd22f9f4abebaad39da18596694102cb5c12f
Patrick Schilf <pschilf@unb.ca>
2021-03-03 12:24:08 -0400

    INTRANET-232 Refactor table rendering

--- modules/custom_entity_ui/src/Controller/EntityTypeListController.php
+++ modules/custom_entity_ui/src/Controller/EntityTypeListController.php
@@ -10,7 +10,7 @@ use Drupal\Core\Entity\EntityTypeInterface;
  *
  * @package Drupal\custom_entity_ui\Controller
  */
-class EntityTypeListController extends ControllerBase {
+abstract class EntityTypeListController extends ControllerBase {
 
   /**
    * Creates a list of entity types.
@@ -18,63 +18,7 @@ class EntityTypeListController extends ControllerBase {
    * @return array
    *   A render element.
    */
-  public function listing() {
-    return [
-      '#type' => 'table',
-      '#header' => $this->buildHeader(),
-      '#rows' => $this->buildRows(),
-      '#empty' => $this->emptyMessage(),
-    ];
-  }
-
-  /**
-   * Builds the header row for the entity type listing.
-   *
-   * @return array
-   *   A render array structure of header strings.
-   *
-   * @see \Drupal\custom_entity_ui\Controller\EntityTypeListController::listing()
-   */
-  protected function buildHeader() {
-    return [
-      'machine_name' => $this->t('Machine name'),
-      'name' => $this->t('Name'),
-      'provider' => $this->t('Defined by'),
-    ];
-  }
-
-  /**
-   * Builds a row for each entity type in the entity type listing.
-   *
-   * @return array
-   *   A render array structure of properties for each entity type.
-   *
-   * @see \Drupal\custom_entity_ui\Controller\EntityTypeListController::listing()
-   */
-  protected function buildRows() {
-    $rows = [];
-    foreach ($this->loadEntityTypes() as $entity_type) {
-      $rows[] = $this->buildRow($entity_type);
-    }
-    return $rows;
-  }
-
-  /**
-   * Builds a row for an entity type in the entity type listing.
-   *
-   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
-   *   The entity type for this row of the list.
-   *
-   * @return array
-   *   A render array structure of properties for the given entity type.
-   */
-  protected function buildRow(EntityTypeInterface $entity_type) {
-    return [
-      'id' => $entity_type->id(),
-      'name' => $entity_type->getLabel(),
-      'provider' => $entity_type->getProvider(),
-    ];
-  }
+  abstract public function listing();
 
   /**
    * Load the entity types to contain in this listing.
@@ -98,16 +42,6 @@ class EntityTypeListController extends ControllerBase {
    */
   protected function filter(EntityTypeInterface $entity_type) {
     return $entity_type->getGroup() === 'content';
-  }
-
-  /**
-   * The message to display when the list contains no entries.
-   *
-   * @return \Drupal\Core\StringTranslation\TranslatableMarkup|string
-   *   A (translatable) string.
-   */
-  protected function emptyMessage() {
-    return $this->t('No custom entity types have been defined.');
   }
 
 }
