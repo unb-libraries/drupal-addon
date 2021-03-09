53c999c1cd093185b99db633e1252f96516e71ad
Patrick Schilf <pschilf@unb.ca>
2021-03-09 12:47:16 -0400

    INTRANET-232 Entity type settings form

--- modules/custom_entity_ui/src/Form/EntityTypeSettingsForm.php
+++ modules/custom_entity_ui/src/Form/EntityTypeSettingsForm.php
@@ -3,6 +3,7 @@
 namespace Drupal\custom_entity_ui\Form;
 
 use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -11,41 +12,78 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *
  * @package Drupal\custom_entity_ui\Form
  */
-class EntityTypeSettingsForm implements EntityTypeSettingsFormInterface {
+class EntityTypeSettingsForm extends FormBase implements EntityTypeSettingsFormInterface {
+
+  /**
+   * The entity type.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeInterface
+   */
+  protected $entityType;
+
+  /**
+   * Get the entity type.
+   *
+   * @return \Drupal\Core\Entity\EntityTypeInterface
+   *   An entity type.
+   */
+  protected function getEntityType() {
+    return $this->entityType;
+  }
+
+  /**
+   * EntityTypeSettingsForm constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   *   An entity type.
+   */
+  public function __construct(EntityTypeInterface $entity_type) {
+    $this->entityType = $entity_type;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container) {
+    $route_matcher = $container->get('current_route_match');
+    $entity_type_id = $route_matcher
+      ->getRouteObject()
+      ->getDefault('entity_type_id');
+    $entity_type = $container->get('entity_type.manager')
+      ->getDefinition($entity_type_id);
+
+    return new static($entity_type);
+  }
 
   /**
    * {@inheritDoc}
    */
   public function getFormId() {
-    // TODO: Implement getFormId() method.
+    $entity_type_id = $this->getEntityType()->id();
+    return "{$entity_type_id}.settings_form";
   }
 
   /**
    * {@inheritDoc}
    */
   public function getTitle() {
-    // TODO: Implement getTitle() method.
+    return $this->t('@entity_type Settings', [
+      '@entity_type' => $this->getEntityType()->getLabel(),
+    ]);
   }
 
   /**
    * {@inheritDoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    // TODO: Implement buildForm() method.
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    // TODO: Implement validateForm() method.
+    return $form;
   }
 
   /**
    * {@inheritDoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    // TODO: Implement submitForm() method.
   }
+
 
 }
