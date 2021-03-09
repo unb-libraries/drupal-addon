4424e6dd55c428dd42a11f025cf77163f3ac0dfe
Patrick Schilf <pschilf@unb.ca>
2021-03-09 12:28:37 -0400

    INTRANET-232 Add dynamic entity settings form routes

---
+++ modules/custom_entity_ui/src/Form/EntityTypeSettingsForm.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\custom_entity_ui\Form;
+
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Form controller for entity type settings forms.
+ *
+ * @package Drupal\custom_entity_ui\Form
+ */
+class EntityTypeSettingsForm implements EntityTypeSettingsFormInterface {
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getFormId() {
+    // TODO: Implement getFormId() method.
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getTitle() {
+    // TODO: Implement getTitle() method.
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    // TODO: Implement buildForm() method.
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    // TODO: Implement validateForm() method.
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // TODO: Implement submitForm() method.
+  }
+
+}
