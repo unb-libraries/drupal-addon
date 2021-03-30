cf3c1259c03732808344145873f1e04ac87b02e9
Patrick Schilf <pschilf@unb.ca>
2021-03-30 15:29:50 -0300

    [lib_unb_custom_entity] Fix overriding entity admin permission

--- modules/lib_unb_custom_entity/src/Entity/EntityAccessControlHandler.php
+++ modules/lib_unb_custom_entity/src/Entity/EntityAccessControlHandler.php
@@ -5,6 +5,7 @@ namespace Drupal\lib_unb_custom_entity\Entity;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Entity\EntityAccessControlHandler as DefaultEntityAccessControlHandler;
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Session\AccountInterface;
 
 /**
@@ -42,17 +43,43 @@ class EntityAccessControlHandler extends DefaultEntityAccessControlHandler {
    */
   protected function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
     $access = parent::checkAccess($entity, $operation, $account);
-    if (!$access->isForbidden()) {
-      $account = $this->prepareUser($account);
 
-      $entity_type_id = $entity->getEntityTypeId();
-      $required_permission = "$operation $entity_type_id entities";
+    // If explicit access has already been granted, it means the user has admin
+    // permissions. If access has been explicitly denied, further checks are
+    // equally redundant.
+    if ($access->isNeutral()) {
+      $account = $this->prepareUser($account);
+      $has_access = $this->hasEntityTypePermission($entity->getEntityType(), $operation, $account);
+
       // @todo Cache access checking.
-      $access = AccessResult::allowedIfHasPermission(
-        $account, $required_permission);
+      $access = $has_access
+        ? AccessResult::allowed()
+        : AccessResult::forbidden();
     }
 
     return $access;
+  }
+
+  /**
+   * Whether access should be granted based on the given entity type.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   *   The entity type.
+   * @param string $operation
+   *   A string.
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   A user account.
+   *
+   * @return bool
+   *   TRUE if the account has the permission to perform the given operation on
+   *   the given entity type, i.e. "edit node entities" for a user who
+   *   asks for permission to "edit" a "node" entity.
+   *   FALSE otherwise.
+   */
+  protected function hasEntityTypePermission(EntityTypeInterface $entity_type, string $operation, AccountInterface $account) {
+
+    $required_permission = "$operation {$entity_type->id()} entities";
+    return $account->hasPermission($required_permission);
   }
 
   /**
