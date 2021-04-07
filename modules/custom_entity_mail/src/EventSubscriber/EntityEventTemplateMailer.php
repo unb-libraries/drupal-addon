a945d36f3dd84558dca7c9638a8c832d6b14ed8c
Patrick Schilf <pschilf@unb.ca>
2021-04-07 12:21:18 -0300

    INTRANET-224 Provide entity URL in template context

--- modules/custom_entity_mail/src/EventSubscriber/EntityEventTemplateMailer.php
+++ modules/custom_entity_mail/src/EventSubscriber/EntityEventTemplateMailer.php
@@ -3,6 +3,8 @@
 namespace Drupal\custom_entity_mail\EventSubscriber;
 
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Mail\MailManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Event subscriber sending template-based emails upon entity events.
@@ -10,6 +12,38 @@ use Drupal\Core\Entity\EntityInterface;
  * @package Drupal\custom_entity_mail\EventSubscriber
  */
 abstract class EntityEventTemplateMailer extends EntityEventMailer {
+
+  /**
+   * The host and scheme part of the entity URL.
+   *
+   * @var string
+   */
+  protected $entityUrlBase;
+
+  /**
+   * Get the host and scheme part of the entity URL.
+   *
+   * @return string
+   *   A string, e.g. https://example.com.
+   */
+  protected function getEntityUrlBase() {
+    return $this->entityUrlBase;
+  }
+
+  /**
+   * EntityEventTemplateMailer constructor.
+   *
+   * @param \Drupal\Core\Mail\MailManagerInterface $mail_manager
+   *   A mail manager instance.
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The current request.
+   * @param string $entity_type_id
+   *   A string.
+   */
+  public function __construct(MailManagerInterface $mail_manager, Request $request, string $entity_type_id) {
+    parent::__construct($mail_manager, $entity_type_id);
+    $this->entityUrlBase = $request->getSchemeAndHttpHost();
+  }
 
   /**
    * {@inheritDoc}
@@ -108,8 +142,10 @@ abstract class EntityEventTemplateMailer extends EntityEventMailer {
    *   An array of variable names and values.
    */
   protected function getBodyContext(EntityInterface $entity, string $key) {
+    $entity_url = $this->getEntityUrlBase() . $entity->toUrl()->toString();
     return [
       $entity->getEntityTypeId() => $entity,
+      $entity->getEntityTypeId() . '_url' => $entity_url,
     ];
   }
 
