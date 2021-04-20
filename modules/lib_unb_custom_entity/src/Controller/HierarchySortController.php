3ec73b1ed0a3d8e492c6234ff2e4328a9b10e775
Patrick Schilf <pschilf@unb.ca>
2021-04-20 12:31:25 -0300

    [lib_unb_custom_entity] Controller for (re-)sorting hierarchical entities

---
+++ modules/lib_unb_custom_entity/src/Controller/HierarchySortController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Drupal\lib_unb_custom_entity\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\lib_unb_custom_entity\Entity\SortableHierarchicalInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * (Re-)Calculates a hierarchical entity's sort order.
+ *
+ * @package Drupal\lib_unb_custom_entity\Controller
+ */
+class HierarchySortController extends ControllerBase {
+
+  /**
+   * The entity type.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeInterface
+   */
+  protected $entityType;
+
+  /**
+   * The entity storage handler.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $entityStorage;
+
+  /**
+   * Get the entity type.
+   *
+   * @return \Drupal\Core\Entity\EntityTypeInterface
+   *   An entity type definition.
+   */
+  protected function getEntityType() {
+    return $this->entityType;
+  }
+
+  /**
+   * Get the entity storage handler.
+   *
+   * @return \Drupal\Core\Entity\EntityStorageInterface
+   *   A entity storage handler.
+   */
+  protected function entityStorage() {
+    return $this->entityStorage;
+  }
+
+  /**
+   * Create a HierarchySortController instance.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   *   An entity type definition.
+   * @param \Drupal\Core\Entity\EntityStorageInterface $entity_storage
+   *   An entity storage handler.
+   */
+  public function __construct(EntityTypeInterface $entity_type, EntityStorageInterface $entity_storage) {
+    $this->entityType = $entity_type;
+    $this->entityStorage = $entity_storage;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container) {
+    $current_route = $container->get('current_route_match')
+      ->getRouteObject();
+    list($prefix, $entity_type_id) = explode(':', $current_route
+      ->getOption('parameters')['entity_type_id']['type']);
+    $entity_type_manager = $container->get('entity_type.manager');
+    $entity_type = $entity_type_manager
+      ->getDefinition($entity_type_id);
+    $storage = $entity_type_manager
+      ->getStorage($entity_type_id);
+    return new static($entity_type, $storage);
+  }
+
+  /**
+   * Sort entities of the type assigned to the controller.
+   *
+   * @return \Symfony\Component\HttpFoundation\RedirectResponse
+   *   A redirect response. The redirect destination is the "collection"
+   *   route of the entity type assigned to the controller.
+   */
+  public function sort() {
+    /** @var \Drupal\lib_unb_custom_entity\Entity\SortableHierarchicalInterface $sortable_hierarchical_entity */
+    foreach ($this->entityStorage()->loadMultiple() as $sortable_hierarchical_entity) {
+      $sortable_hierarchical_entity->get(SortableHierarchicalInterface::FIELD_SORT_KEY)
+        ->applyDefaultValue();
+      $sortable_hierarchical_entity->save();
+    }
+    return $this->redirect("entity.{$this->getEntityType()->id()}.collection");
+  }
+
+}
